a7ec658112045964038654dd1a3d27cdfc7a6274
Jack Hannington <hannington94@gmail.com>
2023-05-01 22:54:35 +0100

    added new route

--- index.js
+++ index.js
@@ -13,9 +13,11 @@ const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({ extended: true }));
+
+
 //setup public folder
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 //select all data from matches table
 async function getMatches() {
@@ -29,10 +31,25 @@ async function getMatches() {
     return matches;
 }
 
-
+async function addFixture(fixture) {
+    const { data, error } = await supabase
+      .from('matches')
+      .insert([
+        fixture
+      ]);
+  
+    if (error) {
+      console.error('Error adding fixture:', error);
+      return null;
+    }
+  
+    // If data is null or empty, return the input fixture instead of data[0]
+    return data && data.length > 0 ? data[0] : fixture;
+  }
+  
+  
 
 app.get('/', async (req, res) => {
-    // Replace mockData with the data fetched from the database
     const matches = await getMatches();
   
     const fixturesByMonth = matches.reduce((acc, fixture) => {
@@ -45,6 +62,31 @@ app.get('/', async (req, res) => {
   
     res.render('index', { fixturesByMonth: Object.values(fixturesByMonth) });
   });
+
+  //new route
+  app.get('/new', (req, res) => {
+    res.render('new');
+  });
+  
+
+  app.post('/fixtures/new', async (req, res) => {
+    const newFixture = req.body;
+    const addedFixture = await addFixture(newFixture);
+  
+    if (addedFixture) {
+      res.redirect('/');
+    } else {
+      res.status(500).send('Error adding fixture');
+    }
+  });
+  
+  
+
+//edit route
+app.get('/edit', async (req, res) => {
+        const matches = await getMatches();
+        res.render('edit', { matches });
+});
   
 
 app.listen(port, () => {
