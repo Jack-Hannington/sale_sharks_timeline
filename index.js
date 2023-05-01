f9feaee5c09b7dd8c0e5d8c47f13f8f94323c02d
Jack Hannington <hannington94@gmail.com>
2023-05-01 22:22:17 +0100

    database integrated

--- index.js
+++ index.js
@@ -1,61 +1,51 @@
 const express = require('express');
 const ejs = require('ejs');
 const app = express();
-const port = 3000;
+require('dotenv').config();
+const port = process.env.PORT;
 const path = require('path');
 
+// change from import
+const { createClient } = require('@supabase/supabase-js')
+
+const supabaseUrl = 'https://cdduewtpfvhhhogaybjk.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 app.set('view engine', 'ejs');
 
 //setup public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-const mockData = [{
-    "month": "August 2022",
-    "opponent": "Bath",
-    "result": "Win"
-  },
-  {
-    "month": "August 2022",
-    "opponent": "Saracens",
-    "result": "Win",
-    "report": "A victory"
-  },
-  {
-    "month": "September 2022",
-    "opponent": "Northampton",
-    "result": "Win"
-  },
-  {
-    "month": "September 2022",
-    "opponent": "Exeter",
-    "result": "Loss",
-    "report": "A victory"
-  },
-  {
-    "month": "October 2022",
-    "opponent": "Harlequins",
-    "result": "Win"
-  },
-  {
-    "month": "October 2022",
-    "opponent": "Bristol",
-    "result": "Draw",
-    "report": "A draw"
-  }
-];
-
-app.get('/', (req, res) => {
-  const fixturesByMonth = mockData.reduce((acc, fixture) => {
-    if (!acc[fixture.month]) {
-      acc[fixture.month] = { month: fixture.month, fixtures: [] };
+//select all data from matches table
+async function getMatches() {
+    let { data: matches, error } = await supabase
+        .from('matches')
+        .select('*');
+    
+    if (error) {
+        console.error('Error fetching matches:', error);
     }
-    acc[fixture.month].fixtures.push(fixture);
-    return acc;
-  }, []);
+    return matches;
+}
 
-  res.render('index', { fixturesByMonth: Object.values(fixturesByMonth) });
-});
+
+
+app.get('/', async (req, res) => {
+    // Replace mockData with the data fetched from the database
+    const matches = await getMatches();
+  
+    const fixturesByMonth = matches.reduce((acc, fixture) => {
+      if (!acc[fixture.month]) {
+        acc[fixture.month] = { month: fixture.month, fixtures: [] };
+      }
+      acc[fixture.month].fixtures.push(fixture);
+      return acc;
+    }, []);
+  
+    res.render('index', { fixturesByMonth: Object.values(fixturesByMonth) });
+  });
+  
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
